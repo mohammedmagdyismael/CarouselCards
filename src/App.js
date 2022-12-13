ee680a7e8f8dd5b096fe562f6d1151bee76a5004
mohamed magdy ismael <mohamed.magdy@vezeeta.com>
2022-12-13 20:21:09 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CarousalGallery from './CarouselCards';
+import './App.css'
+
+const App = ({ ...props }) => {
+    return (
+        <div>
+            <CarousalGallery />
+         </div>
+                
+    )
+}
+
+export default App;
